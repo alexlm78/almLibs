70ad404d33b7f4ff30068a6314d388b114d4df3a
Alejandro Lopez Monzon <alexlm78@gmail.com>
2017-02-09 15:30:29 -0600

    Add BarCode generation example.

---
+++ src/org/alexlm78/utils/BarCodes.java
@@ -0,0 +1,39 @@
+package org.alexlm78.utils;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import net.sourceforge.jbarcodebean.JBarcodeBean;
+import net.sourceforge.jbarcodebean.model.Code39;
+//import net.sourceforge.jbarcodebean.model.Interleaved25;
+
+public class BarCodes 
+{
+	public static void testBarCode() 
+	{
+		try 
+		{
+			JBarcodeBean barcode = new JBarcodeBean();
+	
+	        // nuestro tipo de codigo de barra
+	        //barcode.setCodeType(new Interleaved25());
+	        barcode.setCodeType(new Code39());
+	
+	        // nuestro valor a codificar y algunas configuraciones mas
+	        barcode.setCode("1234554321987654321123456789");
+	        barcode.setCheckDigit(true);
+	
+	        BufferedImage bufferedImage = barcode.draw(new BufferedImage(400, 400, BufferedImage.TYPE_INT_RGB));
+	
+	        // guardar en disco como png
+	        File file = new File("C:\\Temps\\codebar.png");
+        
+			ImageIO.write(bufferedImage, "png", file);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+    }
+
+}
